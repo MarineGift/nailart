578e5c900b363be36ce407ff57e73e1d02da6885
MarineGift <marinegift4u@gmail.com>
2025-08-27 12:17:06 -0400

    a

--- components/customer-sheet-management.tsx
+++
@@ -1,7 +0,0 @@
-'use client'
-
-import EnhancedCustomerManagement from './enhanced-customer-management'
-
-export function CustomerSheetManagement() {
-  return <EnhancedCustomerManagement onBack={() => {}} />
-}
